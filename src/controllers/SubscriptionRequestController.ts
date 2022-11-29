83d670fcea893cc429c773b0c2ea45f8cb34c9d8
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-11-29 13:46:27 +0700

    feat: connect soap network to rest

--- src/controllers/SubscriptionRequestController.ts
+++ src/controllers/SubscriptionRequestController.ts
@@ -8,20 +8,23 @@ const soap = require('soap');
 class SubscriptionRequestController extends BaseController {
     getSubscriptionRequests = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const url = 'http://localhost:3101/SubscriptionService?wsdl';
+            const iplocal = "192.168.127.83"; //pake iplocal masing2 pake command ipconfig
+            const url = 'http://192.168.127.83:3101/SubscriptionService?wsdl';
             var client = await soap.createClientAsync(url);
             let limit : number;
             let offset : number;
-            if(req.query.limit && typeof req.query.limit === 'string'){
-                limit = parseInt(req.query.limit,10);
-            } else {
-                limit = 10;
-            }
-            if(req.query.offset && typeof req.query.offset === 'string'){
-                offset = parseInt(req.query.offset,10);
-            } else {
-                offset = 0;
-            }
+            // if(req.query.limit && typeof req.query.limit === 'string'){
+            //     limit = parseInt(req.query.limit,10);
+            // } else {
+            //     limit = 10;
+            // }
+            limit = parseInt(req.query.limit as string) || 10;
+            offset = parseInt(req.query.offset as string) || 0;
+            // if(req.query.offset && typeof req.query.offset === 'string'){
+            //     offset = parseInt(req.query.offset,10);
+            // } else {
+            //     offset = 0;
+            // }
             let args = {
                 offset: offset,
                 limit: limit,
@@ -29,8 +32,8 @@ class SubscriptionRequestController extends BaseController {
             }
             var result = await client.getRequestsAsync(args);
             console.log(result);
-            res.status(200).json(result);
-        } catch (error) {
+            res.status(200).json(result[0]);
+        } catch (error) { 
             console.log(error);
             res.status(500).send({ message: "Internal Server Error" })
         }
@@ -38,11 +41,12 @@ class SubscriptionRequestController extends BaseController {
 
     respondRequestSubs = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const url = 'http://localhost:3101/SubscriptionService?wsdl';
+            const url = 'http://192.168.127.83:3101/SubscriptionService?wsdl'; // kyknya harus pake local ip masing2 
             var client = await soap.createClientAsync(url);
             let args = {
                 ...req.body
             }
+            console.log(req.body);
             var result = await client.respondRequestSubsAsync(args);
             console.log(result);
             res.status(200).json(result);
