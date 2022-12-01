28e883e9ef3765c93a09683d05a45d9b64c8a087
firizky29 <13520095@std.stei.itb.ac.id>
2022-12-01 14:44:37 +0700

    feat: added soap_api_key

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -6,4 +6,5 @@ export const CONST = {
     MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
     MYSQL_DATABASE: process.env.MYSQL_DATABASE,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    SOAP_API_KEY: process.env.SOAP_API_KEY,
 }
