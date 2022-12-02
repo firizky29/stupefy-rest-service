dea895cb83eeb0cc48e89a79f985ab2e67f0826a
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-12-02 17:14:41 +0700

    fix: change api getRequests

--- src/controllers/SubscriptionRequestController.ts
+++ src/controllers/SubscriptionRequestController.ts
@@ -10,10 +10,11 @@ class SubscriptionRequestController extends BaseController {
     getSubscriptionRequests = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const ip_soap = "stupefy-soap-service-server-1"; //pake iplocal masing2 pake command ipconfig
-            const iplocal = "192.168.100.120";
             // const url = `http://${ip_soap}:3101/SubscriptionService?wsdl`;
-            const url = `http://192.168.100.120:3101/SubscriptionService?wsdl`;
+            // const url = `http://${CONST.IP_LOCAL}:3101/SubscriptionService?wsdl`;
+            const url = `http://192.168.0.189:3101/SubscriptionService?wsdl`;
             var client = await soap.createClientAsync(url);
+            var client1 = await soap.createClientAsync(url);
             let limit : number;
             let offset : number;
             let page: number;
@@ -22,16 +23,22 @@ class SubscriptionRequestController extends BaseController {
             // } else {
             //     limit = 10;
             // }
-            const totalData = await client.getRequestsAsync({
+            const totalData = await client1.getRequestsAsync({
                 offset:0,
                 limit:999999999,
                 apiKey: CONST.SOAP_API_KEY,
             })
-            const total = totalData[0].length;
+            // const totalData_json = await totalData[0].json();
+            let total =0;
+            if(totalData[0].RequestResponse) {
+            total = totalData[0].RequestResponse.data.length;
+            }
+            console.log(total);
 
             limit = parseInt(req.query.limit as string) || 10;
             page = parseInt(req.query.page as string) || 1;
             offset = limit*(page-1);
+            // offset = parseInt(req.query.offset as string) || 0;
             // if(req.query.offset && typeof req.query.offset === 'string'){
             //     offset = parseInt(req.query.offset,10);
             // } else {
@@ -44,7 +51,9 @@ class SubscriptionRequestController extends BaseController {
             }
             var result = await client.getRequestsAsync(args);
             console.log(result);
-            res.status(200).json({data: result, total_page: Math.ceil(total/limit), total_subscriptions: result[0].length});
+            // res.status(200).json({data: result, total_page: Math.ceil(total/limit), total_subscriptions: result[0].length});
+            // res.status(200).json({data: result[0], total_page: Math.ceil(total/limit)});
+            res.status(200).json({data: result[0], totalData: totalData[0]});
         } catch (error) { 
             console.log(error);
             res.status(500).send({ message: "Internal Server Error" })
@@ -55,7 +64,8 @@ class SubscriptionRequestController extends BaseController {
         try {
             const ip_soap = "stupefy-soap-service-server-1"; //pake iplocal masing2 pake command ipconfig
             // const url = `http://${ip_soap}:3101/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
-            const url = `http://192.168.100.120:3101/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
+            // const url = `http://${CONST.IP_LOCAL}:3101/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
+            const url = `http://192.168.0.189:3101/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
             var client = await soap.createClientAsync(url);
             console.log(req.body);
             let args = {
