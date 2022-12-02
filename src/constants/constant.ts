b05d66ef2b7c90c4fe4c39149e4f6c63bbe68e63
firizky29 <13520095@std.stei.itb.ac.id>
2022-12-02 17:56:34 +0700

    feat: moving ip to env

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -7,4 +7,6 @@ export const CONST = {
     MYSQL_DATABASE: process.env.MYSQL_DATABASE,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
     SOAP_API_KEY: process.env.SOAP_API_KEY,
+    SOAP_IP_ADDRESS: process.env.SOAP_IP_ADDRESS,
+    SOAP_PORT: process.env.SOAP_PORT
 }
--- src/controllers/SubscriptionRequestController.ts
+++ src/controllers/SubscriptionRequestController.ts
@@ -9,10 +9,11 @@ const soap = require('soap');
 class SubscriptionRequestController extends BaseController {
     getSubscriptionRequests = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const ip_soap = "stupefy-soap-service-server-1"; //pake iplocal masing2 pake command ipconfig
+            const ip_soap = CONST.SOAP_IP_ADDRESS; //pake iplocal masing2 pake command ipconfig
+            const port_soap = CONST.SOAP_PORT;
             // const url = `http://${ip_soap}:3101/SubscriptionService?wsdl`;
             // const url = `http://${CONST.IP_LOCAL}:3101/SubscriptionService?wsdl`;
-            const url = `http://192.168.0.189:3101/SubscriptionService?wsdl`;
+            const url = `http://${ip_soap}:${port_soap}/SubscriptionService?wsdl`;
             var client = await soap.createClientAsync(url);
             var client1 = await soap.createClientAsync(url);
             let limit : number;
@@ -62,10 +63,11 @@ class SubscriptionRequestController extends BaseController {
 
     respondRequestSubs = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const ip_soap = "stupefy-soap-service-server-1"; //pake iplocal masing2 pake command ipconfig
+            const ip_soap = CONST.SOAP_IP_ADDRESS; //pake iplocal masing2 pake command ipconfig
+            const port_soap = CONST.SOAP_PORT; //pake iplocal masing2 pake command ipconfig
             // const url = `http://${ip_soap}:3101/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
             // const url = `http://${CONST.IP_LOCAL}:3101/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
-            const url = `http://192.168.0.189:3101/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
+            const url = `http://${ip_soap}:${port_soap}/SubscriptionService?wsdl`; // kyknya harus pake local ip masing2 
             var client = await soap.createClientAsync(url);
             console.log(req.body);
             let args = {
